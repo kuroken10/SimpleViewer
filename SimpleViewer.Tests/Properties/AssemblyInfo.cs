95af923e29fb4b8eec91f17251150626f277671b
kuroken10 <103992688+kuroken10@users.noreply.github.com>
2025-06-08 22:52:03 +0900

    Add MSTest project with ImageCache tests

---
+++ SimpleViewer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleViewer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleViewer.Tests")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
